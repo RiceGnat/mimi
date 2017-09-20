5e9952a654133bc39e4ae5aeac10e07cc4da19de
RiceGnat <ricegnat@gmail.com>
2017-09-20 01:50:05 -0500

    show multistream members

--- mimi.js
+++ mimi.js
@@ -142,22 +142,35 @@ function GetStreamInfo(name, callback) {
 
 function BuildEmbed(stream) {
     var last = new Date(stream.last_live);
+    var multistreamers = [];
+    stream.multistream.forEach((element, index) => {
+        if (element.name != stream.name) {
+            multistreamers.push(element.name + (element.online ? " **(online)**" : " *(offline)*") + (element.adult ? " :underage:" : ""));
+        }
+    });
+    var fields = [
+        {
+            name: "Status",
+            value: stream.online ? "Online" : "Offline",
+            inline: true
+        },
+        {
+            name: "Category",
+            value: stream.category,
+            inline: true
+        }
+    ];
+    if (stream.multistream.length > 0) {
+        fields.push({
+            name: "Multistream",
+            value: multistreamers.join("\n")
+        });
+    }
     return {
         title: stream.name,
         url: `http://picarto.tv/${stream.name}`,
         description: stream.title + "\t" + (stream.gaming ? " :video_game:" : "") + (stream.adult ? " :underage:" : "") + (stream.commissions ? " :paintbrush:" : "") + (stream.private ? " :lock:" : ""),
-        fields: [
-            {
-                name: "Status",
-                value: stream.online ? "Online" : "Offline",
-                inline: true
-            },
-            {
-                name: "Category",
-                value: stream.category,
-                inline: true
-            }
-        ],
+        fields: fields,
         thumbnail: { url: `https://picarto.tv/user_data/usrimg/${stream.name.toLowerCase()}/dsdefault.jpg` },
         image: stream.online ? { url: `https://thumb-us1.picarto.tv/thumbnail/${stream.name}.jpg` } : null,
         footer: !stream.online && last.valueOf() != 0 ? { text: `Last online on ${last.toDateString()}` } : null
