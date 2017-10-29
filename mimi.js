3fa322ccfb277fc7a339fd7974d26fd98c489f21
RiceGnat <ricegnat@gmail.com>
2017-10-29 17:54:18 -0500

    option setting and help

--- mimi.js
+++ mimi.js
@@ -3,6 +3,9 @@ var http = require("https");
 var mysql = require("mysql");
 var auth = process.env.BOT_TOKEN ? null : require("./auth.json");
 
+var streamTracker = {};
+var options = {};
+
 // Bot setup
 var bot = new Discord.Client({
     token: process.env.BOT_TOKEN || auth.token,
@@ -92,6 +95,26 @@ bot.on("message", function (user, userID, channelID, message, evt) {
                     }
                 });
                 break;
+            case "set":
+                if (args[0]) {
+                    var key = args[0].toLowerCase();
+                    var value = args[1];
+                    SetOption(channelID, key, value, (error) => {
+                        var message;
+                        if (error) {
+                            message = "<:mimiscratch:372499377928798208> " + error;
+                        }
+                        else {
+                            message = "<:mimivictory:372499377639522325> Option saved";
+                            console.log(`User ${user} set option ${key} to ${value} in channel ${channelID}`);
+                        }
+                        bot.sendMessage({
+                            to: channelID,
+                            message: message,
+                        });
+                    });
+                }
+                break;
             case "mimi":
                 var req = http.get(`https://picarto.tv/images/chat/emoticons/${args[0]}.png`, (res) => {
                     if (res.statusCode == 200) {
@@ -108,6 +131,35 @@ bot.on("message", function (user, userID, channelID, message, evt) {
                     }
                 });
                 req.end();
+                break;
+            case "help":
+                bot.sendMessage({
+                    to: channelID,
+                    embed: {
+                        title: "<:mimigreetings:372499377501241355> Mimi",
+                        description: "Look up and track Picarto streams. Commands apply only to the Discord channel they're entered from.",
+                        fields: [
+                            {
+                                name: "Streams",
+                                value: [
+                                    "`!stream (name)`\tLook up a stream",
+                                    "`!track (name)`\tTrack a stream in this channel",
+                                    "`!tracking`\tShow a list of tracked streams"].join("\n")
+                            },
+                            {
+                                name: "Control",
+                                value: [
+                                    "`!set (option) (value)`",
+                                    "\t`!set notify-limit (time)[s|m|h]`\tLimit stream notifications"].join("\n")
+                            },
+                            {
+                                name: "Emotes",
+                                value: [
+                                    "`!mimi (emote)`\tMimi emotes from Picarto chat"].join("\n")
+                            }
+                        ]
+                    }
+                });
                 break;
         }
     }
@@ -202,9 +254,16 @@ function UntrackStream(streamName, channelID) {
     db.query(`delete from stream_tracking where stream_name="${streamName}" and discord_channel="${channelID}"`);
 }
 
-// Stream tracking
-var streamTracker = {};
+function GetOptions(callback) {
+    db.query(`select discord_channel, json from options;`, callback);
+}
 
+function SaveOptions(channelID) {
+    var optionStr = JSON.stringify(options[channelID]);
+    db.query(`insert into options (discord_channel, json) values ("${channelID}", ?) on duplicate key update json=?;`, [optionStr, optionStr])
+}
+
+// Stream tracking
 function AddToStreamTracker(streamName, channelID) {
     var name = streamName.toLowerCase();
     if (!streamTracker[name])
@@ -230,6 +289,7 @@ function LoadTrackedStreams() {
             results.forEach((row, index) => {
                 AddToStreamTracker(row.stream_name, row.discord_channel);
             });
+            console.log("Loaded tracked streams");
         }
     });
 }
@@ -258,5 +318,46 @@ function PollTrackedStreams() {
     setTimeout(PollTrackedStreams, 5000);
 }
 
+// Bot control
+function LoadOptions() {
+    GetOptions((error, results, fields) => {
+        if (results) {
+            results.forEach((row, index) => {
+                options[row.discord_channel] = JSON.parse(row.json.toString());
+            });
+            console.log("Loaded options");
+        }
+    });
+}
+
+function SetOption(channelID, key, value, callback) {
+    switch (key) {
+        case "notify-limit":
+            if (!value) return callback("Specify a time (eg 30s, 15m, 1h)");
+            var match = value.toLowerCase().match(/^(\d+)([hms]{0,1})$/);
+            if (!match) return callback("Invalid time specified");
+            var duration = match[1];
+            var unit = match[2];
+            if (!unit) unit = "m";
+            switch (unit) {
+                case "h":
+                    duration *= 60;
+                case "m":
+                    duration *= 60;
+                case "s":
+                    duration *= 1000;
+            }
+            if (!options[channelID]) options[channelID] = {};
+            options[channelID][key] = duration;
+            SaveOptions(channelID);
+            break;
+        default:
+            return callback("Unrecognized option");
+    }
+    callback();
+}
+
+// Loadup
 LoadTrackedStreams();
 PollTrackedStreams();
+LoadOptions();
